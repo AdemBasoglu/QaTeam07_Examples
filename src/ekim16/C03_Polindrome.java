aa2a29a33111a060043067eac6520d968d10aabb
Yusuf Renkoglu <30383492+jusrenn@users.noreply.github.com>
2022-10-17 01:14:42 +0300

    Update C03_Polindrome.java
    Daha kisa oldu

--- src/ekim16/C03_Polindrome.java
+++ src/ekim16/C03_Polindrome.java
@@ -4,37 +4,28 @@ public class C03_Polindrome {
     public static void main(String[] args) {
 
         int sayi = 12321;
-        int sayininOrtasi;
-        String sayininIlkYarisi = "";
-        String sayininSonYarisi = "";
 
         String strSayi = String.valueOf(sayi);
+        String sayininIlkYarisi;
+        String sayininSonYarisi;
+        String sayininSonYarisiTers = "";
 
+        sayininIlkYarisi = strSayi.substring(0, strSayi.length()/2);
+        
         if(strSayi.length() % 2 != 0) {
-            sayininOrtasi = (int)(strSayi.length()+1)/2;
+            sayininSonYarisi = strSayi.substring((strSayi.length()+1)/2);
         } else {
-            int sayininOrtasiIndex = (strSayi.length() / 2);
-            strSayi = strSayi.substring(0,sayininOrtasiIndex) + 'l' + strSayi.substring(sayininOrtasiIndex);
+            sayininSonYarisi = strSayi.substring(strSayi.length()/2);
         }
 
-        for (int i = strSayi.length()-1; i >= 0 ; i--) {
-            if(strSayi.charAt(i) == 'l') {
-                break;
-            }
-            sayininIlkYarisi += strSayi.charAt(i);
+        for (int i = sayininSonYarisi.length()-1; i >= 0 ; i--) {
+            sayininSonYarisiTers += sayininSonYarisi.charAt(i);
         }
 
-        for (int i = 0; i < strSayi.length(); i++) {
-            if(strSayi.charAt(i) == 'l') {
-                break;
-            }
-            sayininSonYarisi += strSayi.charAt(i);
-        }
-
-        if(sayininIlkYarisi.equals(sayininSonYarisi)) {
-            System.out.println("Bu sayi bir polindrome sayidir.");
+        if(sayininIlkYarisi.equals(sayininSonYarisiTers)) {
+            System.out.printf("%d bir polindrome sayidir.", sayi);
         } else {
-            System.out.println("Bu sayi bir polindrome sayi degildir!");
+            System.out.printf("%d bir polindrome degildir sayidir.", sayi);
         }
     }
 }
